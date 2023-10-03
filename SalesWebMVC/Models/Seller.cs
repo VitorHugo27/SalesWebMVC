b4845db4db780467a29defa89f2e050ca5f0f64f
VitorHugo27 <88903733+VitorHugo27@users.noreply.github.com>
2023-10-02 21:18:29 -0300

    02102023-001

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,25 +1,30 @@
-﻿namespace SalesWebMVC.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMVC.Models
 {
     public class Seller
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime BirtDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
-        public Department Department { get; set; }
+        public Department Department { get; set; } 
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birtDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BirtDate = birtDate;
+            BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
         }
@@ -29,7 +34,7 @@
             Sales.Add(sr);
         }
 
-        public void RemoveSales (SalesRecord sr)
+        public void RemoveSales(SalesRecord sr)
         {
             Sales.Remove(sr);
         }
--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 using System.Configuration;
 
 
@@ -17,6 +18,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
 ));
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
